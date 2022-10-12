505dd000f03dcf9cdddcb5a4b4f699f025ff5c27
Michelet Saint-Preux <michelet2000@gmail.com>
2022-10-13 02:17:32 +0800

    app.js components/Login.js index.html

---
+++ components/Login.js
@@ -0,0 +1,98 @@
+app.component('login-form', {
+    template:
+    /*html*/
+    `
+    <div class="container mt-5">
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-6">
+                <h2 class="text-center">{{ title }}<h2>
+                <div class="card px-5 py-5" id="formvalidation">
+                    <form class="form-data" v-if="!submitted">
+                        <div class="forms-inputs mb-4"> 
+                            <label>Email or username</label> 
+                            <input 
+                                autocomplete="off" 
+                                type="text" 
+                                v-model="email" 
+                                v-bind:class="{'form-control':true, 'is-invalid' : !validEmail(email) && emailBlured}" 
+                                v-on:blur="emailBlured = true"
+                            >
+                            <div class="invalid-feedback">A valid email is required!</div>
+                        </div>
+                        <div class="forms-inputs mb-4"> 
+                            <label>Password</label> 
+                            <input 
+                                autocomplete="off" 
+                                type="password" 
+                                v-model="password" 
+                                v-bind:class="{'form-control':true, 'is-invalid' : !validPassword(password) && passwordBlured}" 
+                                v-on:blur="passwordBlured = true"
+                            >
+                            <div class="invalid-feedback">Password must be 8 character!</div>
+                        </div>
+                        <div class="mb-3"> 
+                            <button 
+                                v-on:click.stop.prevent="submit" 
+                                class="btn btn-dark w-100"
+                            >
+                            Login
+                            </button> 
+                        </div>
+                    </form>
+                    <div class="success-data" v-else>
+                        <div 
+                            class="text-center d-flex flex-column"
+                        > 
+                            <i class='bx bxs-badge-check'></i> 
+                            <span class="text-center fs-1">You have been logged in <br> Successfully</span> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `,
+    data: function() {
+        return {
+            title: 'Login Form Validation',
+            email : "",
+            emailBlured : false,
+            valid : false,
+            submitted : false,
+            password:"",
+            passwordBlured:false
+        }
+    },
+    methods:{
+ 
+        validate : function(){
+            this.emailBlured = true;
+            this.passwordBlured = true;
+            if( this.validEmail(this.email) && this.validPassword(this.password)){
+                this.valid = true;
+            }
+        },
+ 
+        validEmail : function(email) {
+ 
+            var re = /(.+)@(.+){2,}\.(.+){2,}/;
+            if(re.test(email.toLowerCase())){
+                return true;
+            }
+ 
+        },
+ 
+        validPassword : function(password) {
+            if (password.length > 7) {
+                return true;
+            }
+        },
+ 
+        submit : function(){
+            this.validate();
+            if(this.valid){
+                this.submitted = true;
+            }
+        }
+    }
+})
